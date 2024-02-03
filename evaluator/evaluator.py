14d866ec89c87222a3bc225c2e8301c8bc51f993
Nathan Clonts <nacclonts@gmail.com>
2024-02-03 11:59:00 -0700

    trim message to first and last 2000 characters

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -6,7 +6,9 @@ import requests
 
 load_dotenv('.environment')
 
+EVALUATOR_MODEL = 'gpt-3.5-turbo'
 openai.api_key = os.environ['OPENAI_API_KEY']
+
 
 headers = {
     "Content-Type": "application/json"
@@ -14,10 +16,19 @@ headers = {
 
 
 def evaluate_message_importance(message: Message) -> str:
-    return get_chatgpt_response(message)
-    
+    # limit the message to the first and last 2000 characters
+    content = message['body'][:2000] + '\n...\n' + message['body'][-2000:]
 
-def get_local_llm_response(message: Message):
+    if EVALUATOR_MODEL == 'local':
+        return get_local_llm_response(content)
+
+    elif EVALUATOR_MODEL[0:3] == 'gpt':
+        return get_chatgpt_response(content)
+
+    raise ValueError('Invalid Evaluator model')
+
+
+def get_local_llm_response(content: str):
     history = [
         {
             "role": "system",
@@ -25,13 +36,14 @@ def get_local_llm_response(message: Message):
                 You are an executive assistant working for a Software Engineering Manager named Nathan Clonts.
                 You receive email messages, and evaluate how important they are for Nathan Clonts to address on a scale of 1 to 10.
                 Evaluate the email, and at the very end of your response, put the number of the importance of the email on a scale of 1 to 10.
+                Do not add any text after the importance level.
 
                 Example response: This email is important because it is waiting for a response, and the sender seems urgent. Importance Level: 8
             """
         },
         {
             "role": "user",
-            "content": message['body']
+            "content": content,
         }
     ]
 
@@ -47,7 +59,7 @@ def get_local_llm_response(message: Message):
     assistant_message = response.json()['choices'][0]['message']['content']
     return assistant_message
 
-def get_chatgpt_response(message: Message):
+def get_chatgpt_response(content: str):
     client = openai.OpenAI(
         api_key=os.environ.get("OPENAI_API_KEY"),
     )
@@ -65,9 +77,9 @@ def get_chatgpt_response(message: Message):
             },
             {
                 "role": "user",
-                "content": message['body']
+                "content": content,
             }
         ],
-        model="gpt-4",
+        model=EVALUATOR_MODEL,
     )
     return chat_completion
