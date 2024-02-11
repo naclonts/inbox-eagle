ba786f203fd5cd3b67125400d6a64fb874725eef
Nathan Clonts <nacclonts@gmail.com>
2024-02-11 11:55:29 -0700

    improve styling; add loading spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import { ClipLoader } from 'react-spinners'; // Using react-spinners for the loading indicator
+
 import './index.css'; // This is the CSS file you'll create for styling
 
 const InboxEagle = () => {
-  const [days, setDays] = useState(7);
+  const [days, setDays] = useState(2);
   // emailEvaluations contains objects with the properties: receivedAt, rating, subject, evaluatorResponse, snippet
   const [emailEvaluations, setEmailEvaluations] = useState([]);
+  const [isLoadingEmailEvaluations, setIsLoadingEmailEvaluations] = useState(false);
 
   const handleEvaluateClick = () => {
     // Placeholder for actual evaluation logic
     console.log('Evaluating emails from the last', days, 'days');
+    setIsLoadingEmailEvaluations(true);
     fetch('http://localhost:5020/get-email-evaluations', {
         method: 'POST',
         headers: {
@@ -20,9 +24,11 @@ const InboxEagle = () => {
       .then(data => {
         console.log('Received email evaluations:', data);
         setEmailEvaluations(data.evaluations);
+        setIsLoadingEmailEvaluations(false);
       })
       .catch(error => {
         console.error('Error:', error);
+        setIsLoadingEmailEvaluations(false);
       });
   };
 
@@ -40,31 +46,46 @@ const InboxEagle = () => {
           value={days}
           onChange={(e) => setDays(e.target.value)}
         />
-        <button onClick={handleEvaluateClick}>Evaluate emails</button>
+        <button onClick={handleEvaluateClick} disabled={isLoadingEmailEvaluations}>
+          Evaluate emails
+        </button>
       </div>
-      <table className="emails">
-        <thead>
-          <tr>
-            <th>Received date</th>
-            <th>Rating</th>
-            <th>Subject</th>
-            <th>Evaluation</th>
-            <th>Snippet</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* list each evaluation */}
-          {emailEvaluations.map((evaluation, index) => (
-            <tr key={index}>
-              <td>{evaluation.receivedAt ? new Date(Number(evaluation.receivedAt)).toLocaleDateString() : ''}</td>
-              <td>{evaluation.rating}</td>
-              <td>{evaluation.subject}</td>
-              <td>{evaluation.evaluatorResponse}</td>
-              <td>{evaluation.snippet}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {
+        isLoadingEmailEvaluations
+          ? (
+            <div className="loading">
+              <ClipLoader color={'#000'} loading={true} size={150} />
+              <p>Evaluating emails...</p>
+            </div>
+          )
+          : (
+            emailEvaluations.length === 0
+            ? <p></p>
+            : <table className="emails">
+              <thead>
+                <tr>
+                  <th>Received date</th>
+                  <th>Rating</th>
+                  <th>Subject</th>
+                  <th>Evaluation</th>
+                  <th>Snippet</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* list each evaluation */}
+                {emailEvaluations.map((evaluation, index) => (
+                  <tr key={index}>
+                    <td>{evaluation.receivedAt ? new Date(Number(evaluation.receivedAt)).toLocaleDateString() : ''}</td>
+                    <td>{evaluation.rating}</td>
+                    <td>{evaluation.subject}</td>
+                    <td>{evaluation.evaluatorResponse}</td>
+                    <td>{evaluation.snippet}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )
+      }
     </div>
   );
 };
