76316648a1fce58466bcb53f907cec1b14d571da
Nathan Clonts <nacclonts@gmail.com>
2024-02-21 23:10:46 -0700

    clean up prompts a bit

--- evaluator/prompt_writer.py
+++ evaluator/prompt_writer.py
@@ -9,11 +9,11 @@ def get_message_evaluation_prompt(prompt_config, message_content: str):
             "role": "system",
             "content": f"""
                 ### Objective:
-                You are an executive assistant working for me, a person named {prompt_config['full_name']} who is a {prompt_config['my_role']}.
+                You are an executive assistant working a person named {prompt_config['full_name']} who is a {prompt_config['my_role']}.
                 {get_company_clause(prompt_config)}
 
                 You receive email messages, and evaluate how important they are for {prompt_config['full_name']} on a scale of 1 to 10.
-                Summarize and evaluate the email, listing any specific facts that are important. Ignore irrelevant information such as unsubscribe links.
+                Summarize and evaluate the email, listing any specific facts that are important.
                 At the end of your response, put the number of the importance of the email on a scale of 1 to 10.
                 {get_rating_criteria_clause(prompt_config)}
 
@@ -56,11 +56,11 @@ def get_rating_extraction_prompt(prompt_config, evaluation_content: str):
         {
             "role": "system",
             "content": f"""
-                Take the importance level from the evaluation and put it at the very end of your response.
+                Take the importance level from the evaluation and return it.
                 Return only the number, and do not add any text, words, or punctuation.
 
-                Example input: This message is fairly important, so I assigned it an 8.0 importance level.
-                Example responses: 8.0
+                Example input: This message is fairly important, so I assigned it an 7.0 importance level.
+                Example responses: 7.0
             """
         },
         {
